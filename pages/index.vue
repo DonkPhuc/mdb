85ee9dad5349d09d196698f80a006f56f26c6059
phuc.do <phuc.do@gss-sol.com>
2024-04-03 10:10:04 +0700

    - checkin

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,9 @@
-<script lang="ts" setup>
-const toast = useToast()
-const { data, pending, execute } = await useFetch('/api/dashboard', {
-  method: 'post',
-  body: {
-    token:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE3LCJpYXQiOjE3MTE5NDY5OTcsImV4cCI6MTcxMjU1MTc5N30.wCfjh9MauyAFFKlOKN4SaSh3peNuQPJ4FNRXKE0N-5I',
-  },
-})
-
-watchEffect(() => {
-  console.log(data.value)
-})
-
-const handleCheckIn = async () => {
-  await execute()
-  toast.add({
-    title:
-      data.value?.code === 200
-        ? 'Successfully'
-        : 'Failed, plese check the token',
-  })
-}
-</script>
+<script lang="ts" setup></script>
 
 <template>
   <div class="flex flex-col gap-4">
     <div>
-      <UButton color="gray" :loading="pending" @click="handleCheckIn"
-        >CheckIn</UButton
-      >
+      <UButton color="gray" @click="$router.push('/projects')">CheckIn</UButton>
     </div>
-    <p v-if="data">{{ data }}</p>
   </div>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,16 +1,26 @@
 <script lang="ts" setup>
-const { data, pending, error, refresh } = await useFetch('/api/projects', {
-  method: 'get',
+const { data, error, pending } = await useFetch('/api/dashboard', {
+  method: 'post',
+  lazy: true,
+  body: {
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjE3LCJpYXQiOjE3MTE5NDY5OTcsImV4cCI6MTcxMjU1MTc5N30.wCfjh9MauyAFFKlOKN4SaSh3peNuQPJ4FNRXKE0N-5I',
+  },
 })
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
-    <USkeleton v-if="pending" class="h-8 w-40" />
-    <p v-else-if="error">{{ error.message }}</p>
-    <p v-else-if="data">{{ data }}</p>
     <div>
-      <UButton color="gray" @click="refresh">Button</UButton>
+      <UNotification
+        v-if="!pending && data"
+        :timeout="60000"
+        :title="error ? 'Failed' : 'Successfully'"
+        :description="error ? 'Failed' : 'Successfully'"
+      />
+    </div>
+    <div v-for="(item, index) in data" :key="index">
+      <p>{{ item }}</p>
     </div>
   </div>
 </template>
